508eddaccc8ff0b31c93e008cb0f154633dc4123
coreyjenkins <101101551@student.swin.edu.au>
2018-10-12 00:34:15 +1100

    Update form_display.php

--- form_display.php
+++ form_display.php
@@ -8,7 +8,7 @@
 <h1>Task 4</h1>
 <h2>Test Form Display</h2>
 Name: <?php echo $_POST["firstName"] . " " . $_POST["lastName"]; ?><br>
-Gender: <?php echo $_POST["gender]; ?><br>
+Gender: <?php echo $_POST["gender"]; ?><br>
 Email Address: <?php echo $_POST["email"]; ?>
 Phone Number: <?php echo $_POST["pNumber"]; ?>
 </body>
